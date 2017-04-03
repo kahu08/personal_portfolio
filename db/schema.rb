358ab0c05eaa0fc88233278c2949311168614c7d
kahu08 <peterkahurani@gmail.com>
2017-04-03 08:11:09 +0300

    setting up the blogpost

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329072302) do
+ActiveRecord::Schema.define(version: 20170403045903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "project_name"
