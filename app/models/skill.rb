2efcbaa0bc399f357d3d78764cef34be594fac0a
kahu08 <peterkahurani@gmail.com>
2017-05-09 18:16:56 +0300

    1.Changed Kenyan flag 2.evenly spaced my about 3.Moved the skill language to appear below 4.Put skills in a row 5.Added tests for validation of form inputs

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,6 +2,7 @@ class Skill < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :language ,presence: true
+  validates :image ,presence: true
   has_many :projects
   belongs_to :user
 end
